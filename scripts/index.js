4e06c36d3ea2253fc8e26a60aedaa2de70667d98
Vadim Koshenkov <misterywou@yandex.ru>
2025-03-27 01:12:58 +0300

    ПР-5 на js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
+
+const container = document.querySelector('.content');
+const cardsContainer = container.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
+function createCard(nameValue, linkValue) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    cardElement.querySelector('.card__title').textContent = nameValue;
+    cardElement.querySelector('.card__image').src = linkValue;
+    cardsContainer.append(cardElement);
+    deleteButton.addEventListener('click', deleteCard);
+}
+
 // @todo: Функция удаления карточки
 
+function deleteCard(evt) {
+    evt.target.closest('.card').remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+for (let i = 0; i < initialCards.length; i++) {
+    createCard(initialCards[i].name, initialCards[i].link);
+}
