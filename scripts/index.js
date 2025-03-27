6c2f04cbc9d2ce9baae8e23bc57feec51e0ffcd8
Vadim Koshenkov <misterywou@yandex.ru>
2025-03-27 20:04:27 +0300

    ПР-5. Исправлены замечания по код ревью-1

--- scripts/index.js
+++ scripts/index.js
@@ -9,23 +9,28 @@ const cardsContainer = container.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
-function createCard(nameValue, linkValue) {
+function createCard(nameValue, linkValue, deleteFunction) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
     const deleteButton = cardElement.querySelector('.card__delete-button');
     cardElement.querySelector('.card__title').textContent = nameValue;
     cardElement.querySelector('.card__image').src = linkValue;
-    cardsContainer.append(cardElement);
-    deleteButton.addEventListener('click', deleteCard);
+    cardElement.querySelector('.card__image').alt = `Фотография места: ${nameValue}`;
+    card = cardElement;
+    deleteButton.addEventListener('click', function() {
+        deleteFunction(cardElement)
+    });
+    return card;
 }
 
 // @todo: Функция удаления карточки
 
-function deleteCard(evt) {
-    evt.target.closest('.card').remove();
+function deleteCard(item) {
+    item.remove();
 }
 
 // @todo: Вывести карточки на страницу
 
-for (let i = 0; i < initialCards.length; i++) {
-    createCard(initialCards[i].name, initialCards[i].link);
-}
+initialCards.forEach(function(item) {
+    createCard(item.name, item.link, deleteCard);
+    cardsContainer.append(card);
+});
